65593312d9a7a03fc926f49cfd1a18310b2ac19f
altoria-alter <39277492+altoria-alter@users.noreply.github.com>
2018-06-09 22:18:33 -0300

    Update Validator.java

--- app/src/main/java/com/example/altoria/passwordvalidator/Validator.java
+++ app/src/main/java/com/example/altoria/passwordvalidator/Validator.java
@@ -1,8 +1,5 @@
 package com.example.altoria.passwordvalidator;
 
-
-import java.util.regex.Pattern;
-
 public class Validator {
 
     //First stage
@@ -17,33 +14,6 @@ public class Validator {
         //If the password is less than 8 characters then RuleNo plus 2
         if(psw.length() < 8) {
             RuleMatch = RuleMatch + "8";
-        }
-        return RuleMatch;
-    }
-
-
-    //Second stage
-    public static String PSWSTG2(String psw) {
-        String RuleMatch  = PSWSTG1(psw);
-
-        //checking special character
-        if(psw.indexOf('~') == -1 && psw.indexOf('!') == -1 && psw.indexOf('@') == -1 &&
-                psw.indexOf('#') == -1 && psw.indexOf('$') == -1 && psw.indexOf('%') == -1 &&
-                psw.indexOf('^') == -1 && psw.indexOf('&') == -1 && psw.indexOf('*') == -1 &&
-                psw.indexOf('-') == -1 && psw.indexOf('=') == -1 && psw.indexOf('+') == -1){
-            RuleMatch = RuleMatch + "s";
-        }
-        //checking digit
-        if(!Pattern.compile("[0-9]").matcher(psw).find()){
-            RuleMatch = RuleMatch + "d";
-        }
-        //checking lower case
-        if(!Pattern.compile("[a-z]").matcher(psw).find()){
-            RuleMatch = RuleMatch + "l";
-        }
-        //checking upper case
-        if(!Pattern.compile("[A-Z]").matcher(psw).find()){
-            RuleMatch = RuleMatch + "u";
         }
         return RuleMatch;
     }
