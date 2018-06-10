b17b2b333363af861d70133abf1ed9f44425a657
altoria-alter <39277492+altoria-alter@users.noreply.github.com>
2018-06-09 22:00:26 -0300

    stage1

--- app/src/main/java/com/example/altoria/password_validator/Validator.java
+++ app/src/main/java/com/example/altoria/password_validator/Validator.java
@@ -6,15 +6,14 @@ public class Validator {
         String RuleMatch = "";
         //If the password is "password" no matter lower or upper cass
         //Then RuleNo plus 1
-        if(psw.equalsIgnoreCase("password")) {
-            RuleMatch = RuleMatch + "p" ;
+        if (psw.equalsIgnoreCase("password")) {
+            RuleMatch = RuleMatch + "p";
         }
 
         //If the password is less than 8 characters then RuleNo plus 2
-        if(psw.length() < 8) {
+        if (psw.length() < 8) {
             RuleMatch = RuleMatch + "8";
         }
         return RuleMatch;
     }
-    
 }
